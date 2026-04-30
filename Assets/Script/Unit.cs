02916993f00b2c1714b8dc481f58c78de19aef63
androidns00 <androidns00@gmail.com>
2026-04-30 14:30:54 +0800

    1

---
+++ Assets/Script/Unit.cs
@@ -0,0 +1,30 @@
+using System;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class NewMonoBehaviourScript : MonoBehaviour
+{
+    Vector3 targetPosition;
+    float stopDistance = 0.1f;
+    public float movespeed = 1f;
+    void Start()
+    {
+        Move(new Vector3(10,0,0));  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Vector3.Distance(transform.position,targetPosition)>stopDistance)
+        {
+            Vector3 moveDirection = (targetPosition - transform.position).normalized;
+            transform.position = transform.position + moveDirection * movespeed * Time.deltaTime;
+            
+        }
+    }
+
+    public void Move(Vector3 Position)
+    {
+        targetPosition = Position;
+    }
+}
