da833a19376f2c6061b415349ad549822f89b2c2
androidns00 <androidns00@gmail.com>
2026-05-06 17:03:43 +0800

    拆俩脚本

---
+++ Assets/Script/GetPosition.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class GetPosition : MonoBehaviour
+{
+    public Vector3 GetMouseWorldPosition(LayerMask layerMask)
+    {
+        Camera mainCamera = Camera.main;
+        if (mainCamera == null || Mouse.current == null)
+        {
+            return Vector3.zero;
+        }
+
+        Ray ray = Camera.main.ScreenPointToRay(Mouse.current.position.ReadValue());
+        if (Physics.Raycast(ray, out RaycastHit hit, float.MaxValue, layerMask))
+        {
+            return hit.point;
+        }
+
+        return Vector3.zero;
+    }
+}
--- Assets/Script/MouseWorld.cs
+++ Assets/Script/MouseWorld.cs
@@ -1,22 +1,14 @@
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 public class MouseWorld : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     public GameObject cube;
     public LayerMask gamePlaneMask;
-    void Start()
-    {
+    public GetPosition positionProvider;
 
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        Ray ray = Camera.main.ScreenPointToRay(Mouse.current.position.ReadValue());
-      
-        Physics.Raycast(ray, out RaycastHit hit,float.MaxValue, gamePlaneMask);
-        cube.transform.position = hit.point;
+       
+        cube.transform.position = positionProvider.GetMouseWorldPosition(gamePlaneMask);
     }
 }
