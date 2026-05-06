3c66c8a15534f64391540e3d14fea08e8589a016
androidns00 <androidns00@gmail.com>
2026-05-06 16:09:51 +0800

    鼠标发射射线控制发光体位置及物体遮罩，后处理添加

--- Assets/Script/MouseWorld.cs
+++ Assets/Script/MouseWorld.cs
@@ -1,33 +1,22 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class MouseWorld : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public GameObject cube;
+    public LayerMask gamePlaneMask;
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-       
-        if (Input.GetMouseButtonDown(0))
-        {
-            // 从摄像机发射一条射线，指向鼠标点击的方向
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            RaycastHit hit;
-
-            // 如果射线碰到了任何带有 Collider（碰撞体）的物体
-            if (Physics.Raycast(ray, out hit))
-            {
-                // hit.point 就是碰撞点的世界坐标
-                Vector3 targetPoint = hit.point;
-                Debug.Log("3D 撞击位置: " + targetPoint);
-                
-                // 你甚至可以获取撞到了什么
-                Debug.Log("撞到了: " + hit.collider.name);
-            }
-        } 
+        Ray ray = Camera.main.ScreenPointToRay(Mouse.current.position.ReadValue());
+      
+        Physics.Raycast(ray, out RaycastHit hit,float.MaxValue, gamePlaneMask);
+        cube.transform.position = hit.point;
     }
 }
